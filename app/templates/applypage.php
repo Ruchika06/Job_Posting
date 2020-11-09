502d6009bdbf28c4a4749cef98dbba872d3f472a
Mehak Jain <b18068@students.iitmandi.ac.in>
2020-11-09 19:19:27 +0530

    Changed some layout

--- app/templates/applypage.php
+++ app/templates/applypage.php
@@ -35,21 +35,17 @@ else
 ?>
 
 <?php include_once 'inc/header.php'; ?>
-<div class="container center grey-text">
-		
-			<h4><?php echo $post->title; ?></h4>
-			<p>Created by <?php echo $post->user_id; ?></p>
-			<p><?php echo strftime("%d/%m/%Y",strtotime($post->created_at)); ?></p>
-			<h5>Description:</h5>
-			<p><?php echo $post->description; ?></p>
 
-			<!-- APPlY FORM -->
-			<form action="postview.php" method="POST">
-				
-				<input type="submit" name="apply" value="Apply" class="btn btn-default">
-			</form>
-
-		
-	</div>
+<div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary">Post No:<?php echo $post->id; ?></strong>
+          <h3 class="mb-0"><?php echo $post->title; ?></h3>
+          <div class="mb-1 text-muted"><?php echo strftime("%d/%m/%Y",strtotime($post->created_at)); ?></div>
+          <p class="card-text mb-auto"><?php echo $post->description; ?><br>Contact: <?php echo $post->contact; ?></p>
+          
+        </div>
+        
+      </div>	
 
 <?php include 'inc/footer.php'?>
