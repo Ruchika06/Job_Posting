66f1a2513423fc0e1aac0fcab2b734b59090aa7b
Sahil Garg <72667149+SahilB18138@users.noreply.github.com>
2020-11-24 23:38:16 +0530

    Update Password Frontend Added

---
+++ app/templates/updatepasspage.php
@@ -0,0 +1,25 @@
+<?php include 'inc/header.php'?>
+    <div class="wrapper" style="margin: 0 auto;">
+        <h2>Update Your Password</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password or Code</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($newpassword_err)) ? 'has-error' : ''; ?>">
+                <label>New Password</label>
+                <input type="password" name="newpassword" class="form-control">
+                <span class="help-block"><?php echo $newpassword_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Update">
+            </div>
+        </form>
+    </div>    
+<?php include 'inc/footer.php'?>
